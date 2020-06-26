9f5daf901f6b87ecdc960c8818a83a1fbb79fd47
Jay Oliver <jaykamas@gmail.com>
2020-06-26 04:14:00 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -8,3 +8,14 @@ def toadstool
   status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
+
+def link 
+  status = "Its Dangerous To Go Out Alone! Take This."
+  puts status
+end
+
+def all_phrases 
+  mario()
+  toadstool()
+  link()
+end 
